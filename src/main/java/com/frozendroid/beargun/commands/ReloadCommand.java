f66eb8d78b93148598de9a08619fe3ff60dfcf0e
Vincent Stakenburg <me@frozendroid.com>
2016-03-27 01:27:47 +0100

    updates reload command

--- src/main/java/com/frozendroid/beargun/commands/ReloadCommand.java
+++ src/main/java/com/frozendroid/beargun/commands/ReloadCommand.java
@@ -1,20 +1,25 @@
 package com.frozendroid.beargun.commands;
 
+import com.frozendroid.beargun.Messenger;
 import com.frozendroid.beargun.MinigameManager;
 import com.frozendroid.beargun.configs.ArenaConfig;
 import com.frozendroid.beargun.configs.GunConfig;
 import com.frozendroid.beargun.models.Match;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class ReloadCommand {
 
     public static void run(CommandSender sender)
     {
+        MinigameManager.endAllQueues();
         MinigameManager.endAllMatches();
 
         MinigameManager.reset();
         GunConfig.loadGuns();
         ArenaConfig.loadArenas();
+
+        sender.sendMessage(Messenger.infoMsg("Reloaded!"));
     }
 
 }
